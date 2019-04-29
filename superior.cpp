fc31be6e2cedea1fc28d7106b19975bc1bd0758f
aylencandela <aylencandela@hotmail.com>
2019-04-29 17:02:07 -0300

    Operaciones Binarias

--- superior.cpp
+++ superior.cpp
@@ -26,6 +26,22 @@ double moduloBinomica(Binomica);
 double calcularArgumento(double, double);
 double argumentoEnElPrimerGiro(double);
 
+// Funciones OB
+void ingresoDeDatosBinomica (Binomica&);
+void ingresoDeDatosPolar (Polar&);
+Binomica sumarBinomica (Binomica, Binomica);
+Polar sumarPolar (Polar, Polar);
+void suma();
+Binomica restarBinomica (Binomica, Binomica);
+Polar restarPolar (Polar, Polar);
+void resta();
+Binomica productoBinomica (Binomica, Binomica);
+Polar productoPolar (Polar, Polar);
+void producto();
+Binomica cocienteBinomica (Binomica, Binomica);
+Polar cocientePolar (Polar, Polar);
+void cociente();
+
 // Funcion principal
 
 int main() {
@@ -48,7 +64,10 @@ int main() {
 			case 1:
 				funcionEDT();
 				break;							
-			//case 2:		
+			case 2: 
+				funcionOB();
+				break;						
+			//case 3:		
 			case 5:
 				cout << endl << "Salida con exito.\n\n" << endl;
 				break;
@@ -101,6 +120,46 @@ void funcionEDT() {
 					return; break;
 	}
 	cout<<"\n\n";
+}
+
+void funcionOB (){
+	
+	system("cls");
+
+	int opcion = 0;
+
+	cout <<endl<< "Que operacion desea realizar?: "<<endl;
+	cout <<endl<< "1. Suma" << endl;
+	cout << "2. Resta" << endl;
+	cout << "3. Producto" << endl;
+	cout << "4. Cociente" << endl;
+	
+	cout <<endl<< "Opcion: ";
+	cin >> opcion;
+	
+	system("cls");
+	
+	switch (opcion) {
+		case 1:
+			system("cls");
+			suma();
+			break;
+		case 2:
+			system("cls");
+			resta();
+			break;
+		case 3:
+			system("cls");
+			producto();
+			break;
+		case 4: 
+			system("cls");
+			cociente();
+			break;
+		default:	cout << endl << "Datos incorrectos. Saliendo...\n\n"; // Si no coincide con los criterios de busqueda
+		return; break;
+	}
+	return;
 }
 
 // PASAJES
@@ -171,4 +230,414 @@ void ImprimirBinomica(Binomica b) {
 
 void ImprimirPolar(Polar p) {
 	cout << endl << "Su representacion en forma polar es: " << '[' << p.modulo << ';' << p.argumento << ']'; // [a;b]
+}
+
+//Cargar Datos
+void ingresoDeDatosBinomica (Binomica&b){
+	
+	cout << "Ingrese la parte real numero complejo: ";
+	cin >> b.real;
+	cout << "Ingrese la parte imaginaria del numero complejo: ";
+	cin >> b.imaginaria;
+}
+
+void ingresoDeDatosPolar (Polar&p){
+			
+	cout << endl << "Ingrese el modulo del numero complejo: ";
+	cin >> p.modulo;
+	if (p.modulo < 0) {
+		cout << endl << "El modulo debe ser positivo. Saliendo...\n\n";
+		return;
+	} else {
+		cout << endl << "Ingrese el argumento del numero complejo: ";
+		cin >> p.argumento;
+	}			
+}
+
+//Sumar
+void suma (){
+
+	int modo = 0;
+	Binomica b1, b2, resultadoB;
+	Polar p1, p2, resultadoP;
+	
+	cout<<"¿De que modo desea cargar sus datos? "<<endl;
+	cout<<endl<<"1. Binomica Binomica"<<endl;
+	cout<<"2. Polar Polar"<<endl;	
+	cout<<"3. Binomica Polar"<<endl;
+	cout<<"4. Polar Binomica"<<endl;
+	cout<<endl<<"Modo: ";
+	cin>>modo;		
+
+	switch (modo){
+		case 1: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosBinomica (b1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosBinomica (b2);	
+			
+			resultadoB = sumarBinomica (b1,b2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+			
+		break;
+		
+		case 2:
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p1);
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p2);
+			
+			resultadoP = sumarPolar (p1, p2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (polarABinomica(resultadoP));
+			ImprimirPolar (resultadoP);
+			
+		break;
+		
+		case 3: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosBinomica (b1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosPolar (p2);
+			
+			resultadoB = sumarBinomica (b1,polarABinomica(p2));
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+
+		break;
+		
+		case 4: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosBinomica (b2);
+			
+			resultadoB = sumarBinomica (polarABinomica(p1),b2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+			
+		break;
+		
+		default:	cout << endl << "Datos incorrectos. Saliendo...\n\n"; // Si no coincide con los criterios de busqueda
+					return; break;
+	}
+}
+
+Binomica sumarBinomica (Binomica b1, Binomica b2){
+	
+	Binomica suma;
+	
+	suma.real = b1.real + b2.real;
+	suma.imaginaria = b1.imaginaria + b2.imaginaria;
+	
+	return suma;
+}
+
+Polar sumarPolar (Polar p1, Polar p2){
+	
+	Polar suma;
+	
+	Binomica sumaB = sumarBinomica (polarABinomica(p1), polarABinomica(p2));
+	suma = binomicaAPolar (sumaB);
+	
+	return suma;
+}
+
+//Restar
+void resta (){
+
+	int modo = 0;
+	Binomica b1, b2, resultadoB;
+	Polar p1, p2, resultadoP;
+	
+	cout<<"¿De que modo desea cargar sus datos? "<<endl;
+	cout<<endl<<"1. Binomica Binomica"<<endl;
+	cout<<"2. Polar Polar"<<endl;	
+	cout<<"3. Binomica Polar"<<endl;
+	cout<<"4. Polar Binomica"<<endl;
+	cout<<endl<<"Modo: ";
+	cin>>modo;		
+
+	switch (modo){
+		case 1: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosBinomica (b1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosBinomica (b2);	
+			
+			resultadoB = restarBinomica (b1,b2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+			
+		break;
+		
+		case 2:
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p1);
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p2);
+			
+			resultadoP = restarPolar (p1, p2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (polarABinomica(resultadoP));
+			ImprimirPolar (resultadoP);
+			
+		break;
+		
+		case 3: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosBinomica (b1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosPolar (p2);
+			
+			resultadoB = restarBinomica (b1,polarABinomica(p2));
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+
+		break;
+		
+		case 4: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosBinomica (b2);
+			
+			resultadoB = restarBinomica (polarABinomica(p1),b2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+			
+		break;
+		
+		default:	cout << endl << "Datos incorrectos. Saliendo...\n\n"; // Si no coincide con los criterios de busqueda
+					return; break;
+	}
+}
+
+Binomica restarBinomica (Binomica b1, Binomica b2){
+	
+	Binomica resta;
+	
+	resta.real = b1.real - b2.real;
+	resta.imaginaria = b1.imaginaria - b2.imaginaria;
+	
+	return resta;
+}
+
+Polar restarPolar (Polar p1, Polar p2){
+	
+	Polar resta;
+	
+	Binomica restaB = restarBinomica (polarABinomica(p1), polarABinomica(p2));
+	resta = binomicaAPolar (restaB);
+	
+	return resta;
+}
+
+//Multiplicar
+void producto (){
+
+	int modo = 0;
+	Binomica b1, b2, resultadoB;
+	Polar p1, p2, resultadoP;
+	
+	cout<<"¿De que modo desea cargar sus datos? "<<endl;
+	cout<<endl<<"1. Binomica Binomica"<<endl;
+	cout<<"2. Polar Polar"<<endl;	
+	cout<<"3. Binomica Polar"<<endl;
+	cout<<"4. Polar Binomica"<<endl;
+	cout<<endl<<"Modo: ";
+	cin>>modo;		
+
+	switch (modo){
+		case 1: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosBinomica (b1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosBinomica (b2);	
+			
+			resultadoB = productoBinomica (b1,b2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+			
+		break;
+		
+		case 2:
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p1);
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p2);
+			
+			resultadoP = productoPolar (p1, p2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (polarABinomica(resultadoP));
+			ImprimirPolar (resultadoP);
+			
+		break;
+		
+		case 3: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosBinomica (b1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosPolar (p2);
+			
+			resultadoB = productoBinomica (b1,polarABinomica(p2));
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+
+		break;
+		
+		case 4: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosBinomica (b2);
+			
+			resultadoB = productoBinomica (polarABinomica(p1),b2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+			
+		break;
+		
+		default:	cout << endl << "Datos incorrectos. Saliendo...\n\n"; // Si no coincide con los criterios de busqueda
+					return; break;
+	}
+}
+
+Binomica productoBinomica (Binomica b1, Binomica b2){
+	
+	Binomica producto;
+	
+	producto.real = (b1.real*b2.real) - (b1.imaginaria*b2.imaginaria);
+	producto.imaginaria = (b1.real * b2.imaginaria) + (b1.imaginaria * b2.real) ;
+	
+	return producto;
+}
+
+Polar productoPolar (Polar p1, Polar p2){
+	
+	Polar producto;
+	
+	producto.modulo = p1.modulo * p2.modulo;
+	producto.argumento = p1.argumento + p2.argumento;
+	
+	return producto;
+}
+
+//Dividir
+void cociente () {	
+	
+	int modo = 0;
+	Binomica b1, b2, resultadoB;
+	Polar p1, p2, resultadoP;
+	
+	cout<<"¿De que modo desea cargar sus datos? "<<endl;
+	cout<<endl<<"1. Binomica Binomica"<<endl;
+	cout<<"2. Polar Polar"<<endl;	
+	cout<<"3. Binomica Polar"<<endl;
+	cout<<"4. Polar Binomica"<<endl;
+	cout<<endl<<"Modo: ";
+	cin>>modo;		
+
+	switch (modo){
+		case 1: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosBinomica (b1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosBinomica (b2);	
+			
+			resultadoB = cocienteBinomica (b1,b2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+			
+		break;
+		
+		case 2:
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p1);
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p2);
+			
+			resultadoP = cocientePolar (p1, p2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (polarABinomica(resultadoP));
+			ImprimirPolar (resultadoP);
+			
+		break;
+		
+		case 3: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosBinomica (b1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosPolar (p2);
+			
+			resultadoB = cocienteBinomica (b1,polarABinomica(p2));
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+
+		break;
+		
+		case 4: 
+			cout<<endl<< "Complejo 1: "<<endl;
+			ingresoDeDatosPolar (p1);
+			cout<<endl<< "Complejo 2: "<<endl;
+			ingresoDeDatosBinomica (b2);
+			
+			resultadoB = cocienteBinomica (polarABinomica(p1),b2);
+			
+			cout<<endl<<"Resultado: ";
+			ImprimirBinomica (resultadoB);
+			ImprimirPolar (binomicaAPolar(resultadoB));
+			
+		break;
+		
+		default:	cout << endl << "Datos incorrectos. Saliendo...\n\n"; // Si no coincide con los criterios de busqueda
+					return; break;
+	}
+}
+
+Binomica cocienteBinomica (Binomica b1, Binomica b2){
+	
+	Binomica cociente;
+	
+	cociente.real = ((b1.real*b2.real) + (b1.imaginaria*b2.imaginaria)) / ((b2.real*b2.real) + (b2.imaginaria*b2.imaginaria));
+	cociente.imaginaria = ((b1.imaginaria*b2.real) - (b1.real*b2.imaginaria)) / ((b2.real*b2.real) + (b2.imaginaria*b2.imaginaria));
+
+	return cociente;
+}
+
+Polar cocientePolar (Polar p1, Polar p2){
+	
+	Polar cociente;
+	
+	cociente.modulo = p1.modulo / p2.modulo;
+	cociente.argumento = p1.argumento - p2.argumento;
+	
+	return cociente;
 }
